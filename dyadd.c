0fbedab3efe6341d2c917cbe85f68c12d6937ea3
AnoyGolui <67983302+AnoyGolui@users.noreply.github.com>
2021-07-17 21:28:53 +0530

    dynamic_addition

---
+++ dyadd.c
@@ -0,0 +1,17 @@
+//Program for dynamic addition
+#include<stdio.h>
+int main()
+{
+  int a,b,ans;
+  printf("Enter 1st number:");
+  scanf("%d",&a);
+
+  printf("Enter 2nd number:");
+  scanf("%d",&b);
+
+  ans = a + b;
+
+  printf("Addition is : %d",ans);
+
+  return 0;
+}
