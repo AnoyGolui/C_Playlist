a713d900ec54f4bf6b87664f43fc9cb5e486d0bb
AnoyGolui <67983302+AnoyGolui@users.noreply.github.com>
2021-07-23 01:33:07 +0530

    Update dyadd.c

--- dyadd.c
+++ dyadd.c
@@ -1,17 +1,18 @@
 //Program for dynamic addition
 #include<stdio.h>
+
 int main()
 {
-  int a,b,ans;
-  printf("Enter 1st number:");
-  scanf("%d",&a);
+  int a, b, ans;
+  printf("Enter 1st number: ");
+  scanf("%d", &a);
 
-  printf("Enter 2nd number:");
-  scanf("%d",&b);
+  printf("Enter 2nd number: ");
+  scanf("%d", &b);
 
   ans = a + b;
 
-  printf("Addition is : %d",ans);
+  printf("Addition is : %d ", ans);
 
   return 0;
 }
